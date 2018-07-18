d916d3f6ea27ac91b3a06f817e54ab687ffcaa73
Michael Lynn <merlynn@gmail.com>
2018-07-18 14:55:18 -0400

    Initial Push - pre-stitch

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: [
+    './src/index.js',
+    'react-hot-loader/patch',
+
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
